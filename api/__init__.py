053b62577b055b5a9fb46116fea3f867f4b98cad
CoolFalcon4 <notcool06@icloud.com>
2025-04-29 10:27:52 -0400

    protect environment variables

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'randomstring'
+    app.config['SECRET_KEY'] = os.environ.get('LAB4_SECRET_KEY')
 
     from .views import views_blueprint
 
